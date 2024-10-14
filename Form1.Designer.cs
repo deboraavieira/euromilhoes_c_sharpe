20307ca1c9a041c9065bac8384da46c2b982f142
Débora Vieira <deboraluizafono@gmail.com>
2024-10-14 10:46:47 +0100

    inicial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace _3939_DeboraVieira_T02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_num1L1 = new System.Windows.Forms.Label();
+            this.lbl_num2L1 = new System.Windows.Forms.Label();
+            this.lbl_num3L1 = new System.Windows.Forms.Label();
+            this.lbl_num4L1 = new System.Windows.Forms.Label();
+            this.lbl_num5L1 = new System.Windows.Forms.Label();
+            this.lbl_num2L2 = new System.Windows.Forms.Label();
+            this.lbl_num1L2 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btn_jogar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Números (1 a 50)";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_num1L1
+            // 
+            this.lbl_num1L1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_num1L1.Location = new System.Drawing.Point(36, 74);
+            this.lbl_num1L1.Name = "lbl_num1L1";
+            this.lbl_num1L1.Size = new System.Drawing.Size(54, 54);
+            this.lbl_num1L1.TabIndex = 1;
+            this.lbl_num1L1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_num2L1
+            // 
+            this.lbl_num2L1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_num2L1.Location = new System.Drawing.Point(107, 74);
+            this.lbl_num2L1.Name = "lbl_num2L1";
+            this.lbl_num2L1.Size = new System.Drawing.Size(54, 54);
+            this.lbl_num2L1.TabIndex = 2;
+            this.lbl_num2L1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_num3L1
+            // 
+            this.lbl_num3L1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_num3L1.Location = new System.Drawing.Point(181, 74);
+            this.lbl_num3L1.Name = "lbl_num3L1";
+            this.lbl_num3L1.Size = new System.Drawing.Size(54, 54);
+            this.lbl_num3L1.TabIndex = 3;
+            this.lbl_num3L1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_num4L1
+            // 
+            this.lbl_num4L1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_num4L1.Location = new System.Drawing.Point(253, 74);
+            this.lbl_num4L1.Name = "lbl_num4L1";
+            this.lbl_num4L1.Size = new System.Drawing.Size(54, 54);
+            this.lbl_num4L1.TabIndex = 4;
+            this.lbl_num4L1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_num5L1
+            // 
+            this.lbl_num5L1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_num5L1.Location = new System.Drawing.Point(323, 74);
+            this.lbl_num5L1.Name = "lbl_num5L1";
+            this.lbl_num5L1.Size = new System.Drawing.Size(54, 54);
+            this.lbl_num5L1.TabIndex = 5;
+            this.lbl_num5L1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_num2L2
+            // 
+            this.lbl_num2L2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_num2L2.Location = new System.Drawing.Point(107, 225);
+            this.lbl_num2L2.Name = "lbl_num2L2";
+            this.lbl_num2L2.Size = new System.Drawing.Size(54, 54);
+            this.lbl_num2L2.TabIndex = 6;
+            this.lbl_num2L2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_num1L2
+            // 
+            this.lbl_num1L2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_num1L2.Location = new System.Drawing.Point(36, 225);
+            this.lbl_num1L2.Name = "lbl_num1L2";
+            this.lbl_num1L2.Size = new System.Drawing.Size(54, 54);
+            this.lbl_num1L2.TabIndex = 7;
+            this.lbl_num1L2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(22, 167);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(172, 25);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Estrelas (1 a 12)";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_jogar
+            // 
+            this.btn_jogar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_jogar.Location = new System.Drawing.Point(266, 221);
+            this.btn_jogar.Name = "btn_jogar";
+            this.btn_jogar.Size = new System.Drawing.Size(175, 54);
+            this.btn_jogar.TabIndex = 9;
+            this.btn_jogar.Text = "Jogar";
+            this.btn_jogar.UseVisualStyleBackColor = true;
+            this.btn_jogar.Click += new System.EventHandler(this.btn_jogar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 324);
+            this.Controls.Add(this.btn_jogar);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lbl_num1L2);
+            this.Controls.Add(this.lbl_num2L2);
+            this.Controls.Add(this.lbl_num5L1);
+            this.Controls.Add(this.lbl_num4L1);
+            this.Controls.Add(this.lbl_num3L1);
+            this.Controls.Add(this.lbl_num2L1);
+            this.Controls.Add(this.lbl_num1L1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "EuroMihões";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_num1L1;
+        private System.Windows.Forms.Label lbl_num2L1;
+        private System.Windows.Forms.Label lbl_num3L1;
+        private System.Windows.Forms.Label lbl_num4L1;
+        private System.Windows.Forms.Label lbl_num5L1;
+        private System.Windows.Forms.Label lbl_num2L2;
+        private System.Windows.Forms.Label lbl_num1L2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btn_jogar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3939_DeboraVieira_T02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_jogar_Click(object sender, EventArgs e)
+        {
+            int[] label1 = new int[5];
+            int[] label2= new int[2];
+            Random rnd = new Random();
+
+
+            for (int i = 0; i < label1.Length; i++)
+            {
+                int num_aleatorio1;
+                do
+                {
+                    num_aleatorio1= rnd.Next(1,51);
+                }while(num_aleatorio1<label1.Length);
+                    label1[i] = num_aleatorio1;
+            }
+            for (int i = 0; i < label2.Length; i++)
+            {
+                int num_aleatorio2;
+                do
+                {
+                    num_aleatorio2 = rnd.Next(1, 13);
+                } while (num_aleatorio2 < label2.Length);
+                label2[i] = num_aleatorio2;
+            }
+
+                    lbl_num1L1.Text = label1[0].ToString();
+                    lbl_num2L1.Text = label1[1].ToString();
+                    lbl_num3L1.Text = label1[2].ToString();
+                    lbl_num4L1.Text = label1[3].ToString();
+                    lbl_num5L1.Text = label1[4].ToString();
+                    lbl_num1L2.Text = label2[0].ToString();
+                    lbl_num2L2.Text = label2[1].ToString();
+            
+
+
+        }
+    }
+}
